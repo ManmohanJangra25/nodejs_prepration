d2c6a1d6d91b0c6b3e9578309729851a31315c3e
Manmohan <manmohanjangra2528@gmail.com>
2024-05-22 02:50:37 +0530

    Done

---
+++ index.js
@@ -0,0 +1,19 @@
+// import $ from "";
+// const generateName = require("sillyname");
+
+import generateName from "sillyname";
+import superheroes from 'superheroes';
+
+var sillyName = generateName();
+
+// fs.writeFile("message.txt", sillyName, (err)=>{
+//     if(err) throw err;
+//     console.log("The file has been saved");
+// });
+
+// fs.readFile("message.txt", 'utf8', (err, data) => {
+//     if(err) throw err;
+//     console.log(data);
+// });
+
+console.log(`I am ${superheroes.random()}`);
